06e4cbc7f7587dc68774f413d59fccc1c48104f8
Onyemobi <onyemobi.anyiwo@gmail.com>
2017-05-04 19:05:40 -0400

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import {Stock} from './stock';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'app works!';
+
+  submitted = false;
+  newStock = new Stock(0, '', '');
+
+
+  onSubmit(){
+    this.submitted = true;
+  }
+
+  cancel(){
+    this.submitted = false;
+    this.newStock = new Stock(0,'','');
+  }
 }
---
+++ src/app/stock.ts
@@ -0,0 +1,7 @@
+export class Stock {
+
+    constructor (public id: number, public stockname: string, public stockCode: string) 
+    {
+
+    }
+}
